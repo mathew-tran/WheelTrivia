9e83aa2e4211ba9353130f2df438b1910a76fbec
Mathew <mathew.tran.dev@gmail.com>
2024-01-18 09:53:54 -0800

    found out inconsistent player joining issue, caused by player spawning on the same point, then it fails to spawn.. needs fixing

---
+++ WheelGame/Source/WheelGame/AGameMode.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WHEELGAME_API AAGameMode : public AGameModeBase
+{
+	GENERATED_BODY()	
+};
---
+++ WheelGame/Source/WheelGame/APlayerStart.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerStart.h"
+#include "APlayerStart.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WHEELGAME_API AAPlayerStart : public APlayerStart
+{
+	GENERATED_BODY()
+	
+};
