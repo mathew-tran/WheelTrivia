140b1886e05371f3cece51805699101b28c484b5
Mathew <mathew.tran.dev@gmail.com>
2024-01-22 16:23:13 -0800

    fix replication issue. Kinda. Set Material having issues when game is replicated. runs fine when locally. Set Overlay Material works when game is replicated. For my needs. I only need to set 1 material, so this is good enough for me

--- WheelGame/Source/WheelGame/AWheelPawn.cpp
+++ WheelGame/Source/WheelGame/AWheelPawn.cpp
@@ -8,6 +8,7 @@ AAWheelPawn::AAWheelPawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
 
 }
 
